810034b43e2ed426bf97b4f7b31f489c2982590f
jcabisado <abisado.jandra@gmail.com>
2015-02-28 11:36:16 +0800

    Commit new db, changed db name in db

--- practice/midterm_test/Abisado_Jandra_midterms/softdev_abisado_jandra_ourspace.sql
+++ practice/midterm_test/Abisado_Jandra_midterms/softdev_abisado_jandra_ourspace.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: Feb 28, 2015 at 02:12 AM
+-- Generation Time: Feb 28, 2015 at 04:34 AM
 -- Server version: 5.6.14
 -- PHP Version: 5.5.6
 
@@ -23,31 +23,44 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Table structure for table `my_address`
+-- Table structure for table `myaddress`
 --
 
-CREATE TABLE IF NOT EXISTS `my_address` (
+CREATE TABLE IF NOT EXISTS `myaddress` (
   `id` int(11) NOT NULL,
   `firstname` varchar(30) NOT NULL,
   `middlename` varchar(30) NOT NULL,
   `lastname` varchar(30) NOT NULL,
-  `gender` int(1) NOT NULL,
-  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+  `gender` varchar(1) NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `my_comment`
+-- Table structure for table `mycomment`
 --
 
-CREATE TABLE IF NOT EXISTS `my_comment` (
-  `id` int(11) NOT NULL,
+CREATE TABLE IF NOT EXISTS `mycomment` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `myaddress_id` int(11) NOT NULL,
   `author` varchar(255) NOT NULL,
   `body` longtext NOT NULL,
-  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`),
+  KEY `mycomment_ibfk_1` (`myaddress_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `mycomment`
+--
+ALTER TABLE `mycomment`
+  ADD CONSTRAINT `mycomment_ibfk_1` FOREIGN KEY (`myaddress_id`) REFERENCES `myaddress` (`id`);
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
