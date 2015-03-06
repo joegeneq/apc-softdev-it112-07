a5eabd041cb669cec664dcb6bbd37ce967b2caa2
jcabisado <abisado.jandra@gmail.com>
2015-03-07 01:04:48 +0800

    Commit updated index

--- application/sdama/frontend/views/site/index.php
+++ application/sdama/frontend/views/site/index.php
@@ -11,29 +11,38 @@ $this->title = 'AMA Foundation';
 
         <!--<p><a class="btn btn-lg btn-success" href="http://www.yiiframework.com">Get started with Yii</a></p>-->
     </div>
-
     <div class="body-content">
 
         <div class="row">
             <div class="col-lg-4">
-                <h2>Who we are?</h2>
-
-                <p>The Associate Missionaries of the Assumption is a community of young volunteers who are committed to transforming society through joyful service deeply rooted in faith and prayer.
-</p>
-
+               
+				<h2>Who we are?</h2>
+				  <?php 
+				$image_url='http://s9.postimg.org/hg3k985rz/image1.jpg';
+				?>
+				<center><img src="<?php echo $image_url;?>" width="350" height="320"><br><br></center>
+                <p>The Associate Missionaries of the Assumption is a community of young volunteers who are committed to transforming society through joyful service deeply rooted in faith and prayer.</p>
+				
                 <!--<p><a class="btn btn-default" href="http://www.yiiframework.com/forum/">Yii Forum &raquo;</a></p>-->
             </div>
 			<div class="col-lg-4">
                 <h2>What is AMA?</h2>
-
+				 <?php 
+				$image_url='http://s9.postimg.org/e9vzmb48v/ama.png';
+				?>
+				<center><img src="<?php echo $image_url;?>" width="350" height="260"><br><br><br><br><br></center>
                 <p>In 1954, the Assumption Sisters founded the Associate Missionaries of the Assumption (AMA) as a way of answering the many appeals of the world. <br><br>Today, the work continues. While at their volunteer sites, AMA's find new insight into God's gifts through service to others. It is through their service that they come to understand more fully the rule of life of The Religious of the Assumption: "Love never remains idle: it makes us creative and impels us to heed all the appeals of the world and to find new ways of answering them." 
 <br><br></p>
 
                 <!--<p><a class="btn btn-default" href="http://www.yiiframework.com/doc/">Read more &raquo;</a></p>-->
             </div>
             <div class="col-lg-4">
+				
                 <h2>What we do?</h2>
-
+				<?php 
+				$image_url='http://s29.postimg.org/5pmnaab5j/image3.jpg';
+				?>
+				<center><img src="<?php echo $image_url;?>" width="350" height="320"><br><br></center>
                 <p>The alumni community continues to be a support group, has provided human & material resources to the program. They oversee implementation & sustainability of the organization.</p>
 
                 <!--<p><a class="btn btn-default" href="http://www.yiiframework.com/extensions/">Yii Extensions &raquo;</a></p>-->
