6c6d51e851fbb5bb6151300b185a9fe2ee30904d
jcabisado <abisado.jandra@gmail.com>
2015-03-07 00:02:16 +0800

    Commit changes in index (home page) and css

--- application/sdama/frontend/views/layouts/main.php
+++ application/sdama/frontend/views/layouts/main.php
@@ -26,7 +26,7 @@ AppAsset::register($this);
     <div class="wrap">
         <?php
             NavBar::begin([
-                'brandLabel' => 'My Company',
+                'brandLabel' => 'AMA Foundation',
                 'brandUrl' => Yii::$app->homeUrl,
                 'options' => [
                     'class' => 'navbar-inverse navbar-fixed-top',
@@ -65,7 +65,7 @@ AppAsset::register($this);
 
     <footer class="footer">
         <div class="container">
-        <p class="pull-left">&copy; My Company <?= date('Y') ?></p>
+        <p class="pull-left">&copy; AMA Foundation <?= date('Y') ?></p>
         <p class="pull-right"><?= Yii::powered() ?></p>
         </div>
     </footer>
