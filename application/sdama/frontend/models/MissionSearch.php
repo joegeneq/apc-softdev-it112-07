a3bc0b45fe833bfcfc66ee756d1abcbb085c5a64
jcabisado <abisado.jandra@gmail.com>
2015-03-07 09:14:15 +0800

    Commit mission crud

---
+++ application/sdama/frontend/models/MissionSearch.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace frontend\models;
+
+use Yii;
+use yii\base\Model;
+use yii\data\ActiveDataProvider;
+use frontend\models\Mission;
+
+/**
+ * MissionSearch represents the model behind the search form about `frontend\models\Mission`.
+ */
+class MissionSearch extends Mission
+{
+    /**
+     * @inheritdoc
+     */
+    public function rules()
+    {
+        return [
+            [['id'], 'integer'],
+            [['mission_name', 'mission_desc', 'mission_num_of_volunteer_needed', 'mission_location', 'mission_contact_person_fname', 'mission_contact_person_lname', 'mission_contact_num', 'mission_email_address'], 'safe'],
+        ];
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function scenarios()
+    {
+        // bypass scenarios() implementation in the parent class
+        return Model::scenarios();
+    }
+
+    /**
+     * Creates data provider instance with search query applied
+     *
+     * @param array $params
+     *
+     * @return ActiveDataProvider
+     */
+    public function search($params)
+    {
+        $query = Mission::find();
+
+        $dataProvider = new ActiveDataProvider([
+            'query' => $query,
+        ]);
+
+        $this->load($params);
+
+        if (!$this->validate()) {
+            // uncomment the following line if you do not want to any records when validation fails
+            // $query->where('0=1');
+            return $dataProvider;
+        }
+
+        $query->andFilterWhere([
+            'id' => $this->id,
+        ]);
+
+        $query->andFilterWhere(['like', 'mission_name', $this->mission_name])
+            ->andFilterWhere(['like', 'mission_desc', $this->mission_desc])
+            ->andFilterWhere(['like', 'mission_num_of_volunteer_needed', $this->mission_num_of_volunteer_needed])
+            ->andFilterWhere(['like', 'mission_location', $this->mission_location])
+            ->andFilterWhere(['like', 'mission_contact_person_fname', $this->mission_contact_person_fname])
+            ->andFilterWhere(['like', 'mission_contact_person_lname', $this->mission_contact_person_lname])
+            ->andFilterWhere(['like', 'mission_contact_num', $this->mission_contact_num])
+            ->andFilterWhere(['like', 'mission_email_address', $this->mission_email_address]);
+
+        return $dataProvider;
+    }
+}
---
+++ application/sdama/frontend/views/Mission/_form.php
@@ -0,0 +1,37 @@
+<?php
+
+use yii\helpers\Html;
+use yii\widgets\ActiveForm;
+
+/* @var $this yii\web\View */
+/* @var $model frontend\models\Mission */
+/* @var $form yii\widgets\ActiveForm */
+?>
+
+<div class="mission-form">
+
+    <?php $form = ActiveForm::begin(); ?>
+
+    <?= $form->field($model, 'mission_name')->textInput(['maxlength' => 45]) ?>
+
+    <?= $form->field($model, 'mission_desc')->textInput(['maxlength' => 45]) ?>
+
+    <?= $form->field($model, 'mission_num_of_volunteer_needed')->textInput(['maxlength' => 45]) ?>
+
+    <?= $form->field($model, 'mission_location')->textInput(['maxlength' => 45]) ?>
+
+    <?= $form->field($model, 'mission_contact_person_fname')->textInput(['maxlength' => 45]) ?>
+
+    <?= $form->field($model, 'mission_contact_person_lname')->textInput(['maxlength' => 45]) ?>
+
+    <?= $form->field($model, 'mission_contact_num')->textInput(['maxlength' => 45]) ?>
+
+    <?= $form->field($model, 'mission_email_address')->textInput(['maxlength' => 45]) ?>
+
+    <div class="form-group">
+        <?= Html::submitButton($model->isNewRecord ? 'Create' : 'Update', ['class' => $model->isNewRecord ? 'btn btn-success' : 'btn btn-primary']) ?>
+    </div>
+
+    <?php ActiveForm::end(); ?>
+
+</div>
