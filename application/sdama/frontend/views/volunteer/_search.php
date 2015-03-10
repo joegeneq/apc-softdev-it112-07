5195873bd0f12bed01dfde85607f99f8a8e42527
Kyra Lacuata <kyralacuata>
2015-03-10 19:32:09 +0800

    commit generate model and crud for migration

---
+++ application/sdama/frontend/views/volunteer/_search.php
@@ -0,0 +1,53 @@
+<?php
+
+use yii\helpers\Html;
+use yii\widgets\ActiveForm;
+
+/**
+ * @var yii\web\View $this
+ * @var frontend\models\VolunteerSearch $model
+ * @var yii\widgets\ActiveForm $form
+ */
+?>
+
+<div class="volunteer-search">
+
+    <?php $form = ActiveForm::begin([
+        'action' => ['index'],
+        'method' => 'get',
+    ]); ?>
+
+    <?= $form->field($model, 'id') ?>
+
+    <?= $form->field($model, 'volunteer_status') ?>
+
+    <?= $form->field($model, 'volunteer_code') ?>
+
+    <?= $form->field($model, 'volunteer_fname') ?>
+
+    <?= $form->field($model, 'volunteer_lname') ?>
+
+    <?php // echo $form->field($model, 'volunteer_mname') ?>
+
+    <?php // echo $form->field($model, 'volunteer_bdate') ?>
+
+    <?php // echo $form->field($model, 'volunteer_age') ?>
+
+    <?php // echo $form->field($model, 'volunteer_school_graduated') ?>
+
+    <?php // echo $form->field($model, 'volunteer_year_graduated') ?>
+
+    <?php // echo $form->field($model, 'volunteer_address') ?>
+
+    <?php // echo $form->field($model, 'volunteer_contact_num') ?>
+
+    <?php // echo $form->field($model, 'volunteer_start_date') ?>
+
+    <div class="form-group">
+        <?= Html::submitButton('Search', ['class' => 'btn btn-primary']) ?>
+        <?= Html::resetButton('Reset', ['class' => 'btn btn-default']) ?>
+    </div>
+
+    <?php ActiveForm::end(); ?>
+
+</div>
---
+++ application/sdama/frontend/views/volunteer/view.php
@@ -0,0 +1,49 @@
+<?php
+
+use yii\helpers\Html;
+use yii\widgets\DetailView;
+
+/**
+ * @var yii\web\View $this
+ * @var frontend\models\Volunteer $model
+ */
+
+$this->title = $model->id;
+$this->params['breadcrumbs'][] = ['label' => 'Volunteers', 'url' => ['index']];
+$this->params['breadcrumbs'][] = $this->title;
+?>
+<div class="volunteer-view">
+
+    <h1><?= Html::encode($this->title) ?></h1>
+
+    <p>
+        <?= Html::a('Update', ['update', 'id' => $model->id], ['class' => 'btn btn-primary']) ?>
+        <?= Html::a('Delete', ['delete', 'id' => $model->id], [
+            'class' => 'btn btn-danger',
+            'data' => [
+                'confirm' => 'Are you sure you want to delete this item?',
+                'method' => 'post',
+            ],
+        ]) ?>
+    </p>
+
+    <?= DetailView::widget([
+        'model' => $model,
+        'attributes' => [
+            'id',
+            'volunteer_status',
+            'volunteer_code',
+            'volunteer_fname',
+            'volunteer_lname',
+            'volunteer_mname',
+            'volunteer_bdate',
+            'volunteer_age',
+            'volunteer_school_graduated',
+            'volunteer_year_graduated',
+            'volunteer_address',
+            'volunteer_contact_num',
+            'volunteer_start_date',
+        ],
+    ]) ?>
+
+</div>
