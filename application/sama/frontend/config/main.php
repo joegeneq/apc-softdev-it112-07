d062081e2a8dc8fcf46519ba43bc5a1c4389ed9c
jcabisado <abisado.jandra@gmail.com>
2015-04-01 19:57:24 +0800

    Commit new project (sama) with themes for frontend and backend

---
+++ application/sama/frontend/config/main.php
@@ -0,0 +1,40 @@
+<?php
+$params = array_merge(
+    require(__DIR__ . '/../../common/config/params.php'),
+    require(__DIR__ . '/../../common/config/params-local.php'),
+    require(__DIR__ . '/params.php'),
+    require(__DIR__ . '/params-local.php')
+);
+
+return [
+    'id' => 'app-frontend',
+    'basePath' => dirname(__DIR__),
+    'bootstrap' => ['log'],
+    'controllerNamespace' => 'frontend\controllers',
+    'components' => [
+		'view'=> [
+		'theme'=> [
+		'pathMap'=> [ 
+			'@app/views'=> '@webroot/themes/houses-on-water'],
+		'baseUrl'=> '@web/themes/houses-on-water',
+        ],
+        ],
+        'user' => [
+            'identityClass' => 'common\models\User',
+            'enableAutoLogin' => true,
+        ],
+        'log' => [
+            'traceLevel' => YII_DEBUG ? 3 : 0,
+            'targets' => [
+                [
+                    'class' => 'yii\log\FileTarget',
+                    'levels' => ['error', 'warning'],
+                ],
+            ],
+        ],
+        'errorHandler' => [
+            'errorAction' => 'site/error',
+        ],
+    ],
+    'params' => $params,
+];
---
+++ application/sama/frontend/views/site/about.php
@@ -0,0 +1,43 @@
+<?php
+use yii\helpers\Html;
+
+/* @var $this yii\web\View*/
+
+$this->title = 'About';
+$this->params['breadcrumbs'][] = $this->title;
+?>
+<div class="site-about">
+    <h1><?= Html::encode($this->title) ?></h1>
+	<h3> History </h3>
+
+    <p>In 1954, the Assumption Sisters founded the Associate Missionaries of the Assumption (AMA) as a way of answering the many appeals of the world.</p>
+	
+	<p>Today, the work continues. While at their volunteer sites, AMA's find new insight into God's gifts through service to others. 
+	It is through their service that they come to understand more fully the rule of life of The Religious of the Assumption: 
+	"Love never remains idle: It makes us creative and impels us to heed all the appeals of the world and to find new ways of answering them."</p>
+	
+	<h3> Vision </h3>
+
+    <p>“A multicultural Filipino society reclaiming its basic rights to peace and development in the spirit of faith, love and justice.”</p>
+	
+	<h3> Mission </h3>
+
+    <p>“We seek to uplift, empower and transform the disadvantaged in society towards attaining their full human development, through direct 
+	intervention or in partnership with institutions and communities, as co-stewards of human and environmental resources. Our culturally-sensitive 
+	services are rooted in faith, love and justice.” </p>
+	
+	<h3> Objectives </h3>
+
+    <p>Associate Missionaries of the Assumption (AMA) Foundation aims to help the poor and the oppressed become economically secure, socially responsible 
+	and morally mature by making available: <br>
+
+	- Sustainable agricultural technology, farm implements and tools <br>
+	- Education (formal and non-formal) <br>
+	- Medical assistance, facilities and health care alternatives <br>
+	- Opportunities for leadership and livelihood and skills training <br>
+	- Values/spiritual formation <br>
+	- Relief and rehabilitation of communities affected by the armed conflict in Mindanao</p>
+
+    
+	<!-- <code><?= __FILE__ ?></code> -->
+</div>
