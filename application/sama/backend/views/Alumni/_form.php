4786ab73f9bf60bd41de7637308415895046fd70
abbycabildo09 <abbycabildo09@gmail.com>
2015-04-06 00:33:00 +0800

    commit updated alumni CRUD with dropdowns and date picker

--- application/sama/backend/views/Alumni/_form.php
+++ application/sama/backend/views/Alumni/_form.php
@@ -2,10 +2,12 @@
 
 use yii\helpers\Html;
 use yii\widgets\ActiveForm;
-
+use dosamigos\datepicker\DatePicker;
 /* @var $this yii\web\View */
 /* @var $model backend\models\alumni */
 /* @var $form yii\widgets\ActiveForm */
+
+
 ?>
 
 <div class="alumni-form">
@@ -18,15 +20,33 @@ use yii\widgets\ActiveForm;
 
     <?= $form->field($model, 'alumni_mname')->textInput(['maxlength' => 45]) ?>
 
-    <?= $form->field($model, 'alumni_gender')->textInput(['maxlength' => 6]) ?>
+    <?= $form->field($model, 'alumni_gender')->dropDownList([
+				''=> '', 'Male' => 'Male', 'Female' => 'Female' ])
+				->label('Gender') ?>
 
-    <?= $form->field($model, 'alumni_birthdate')->textInput() ?>
+    <?= $form->field($model, 'alumni_birthdate')->widget(
+	DatePicker::className(), [
+        // inline too, not bad
+         'inline' => false, 
+		 'options' => ['placeholder' => 'Select date of birth...'],
+         // modify template for custom rendering
+        //'template' => '<div class="well well-sm" style="background-color: #fff; width:250px">{input}</div>',
+        'clientOptions' => [
+            'autoclose' => true,
+            'format' => 'dd-M-yyyy'
+        ]
+	]);?>
 
-    <?= $form->field($model, 'alumni_nationality')->textInput(['maxlength' => 45]) ?>
+    <?= $form->field($model, 'alumni_nationality')->dropDownList([
+				''=> '', 'American' => 'American', 'Filipino' => 'Filipino', 'Indian' => 'Indian', 
+				'Chinese' => 'Chinese', 'Japanese' => 'Japanese', 'Korean' => 'Korean' ])
+				->label('Nationality') ?> 
 
     <?= $form->field($model, 'alumni_occupation')->textInput(['maxlength' => 45]) ?>
 
-    <?= $form->field($model, 'alumni_marital_stat')->textInput(['maxlength' => 10]) ?>
+    <?= $form->field($model, 'alumni_marital_stat')->dropDownList([
+				''=> '', 'Single' => 'Single', 'Married' => 'Married', 'Widowed' => 'Widowed', 'Separated' => 'Separated' ])
+				->label('Marital Status') ?>
 
     <?= $form->field($model, 'alumni_address')->textInput(['maxlength' => 45]) ?>
 
@@ -38,7 +58,10 @@ use yii\widgets\ActiveForm;
 
     <?= $form->field($model, 'alumni_school_graduated_from')->textInput(['maxlength' => 45]) ?>
 
-    <?= $form->field($model, 'alumni_school_year_graduated')->textInput(['maxlength' => 45]) ?>
+    <?= $form->field($model, 'alumni_school_year_graduated')->dropDownList([
+				''=> '', '1970' => '1970', '1980' => '1980', '1990' => '1990', 
+				'2000' => '2000', '2010' => '2010', '2014' => '2014' ])
+				->label('School Year Graduated') ?> 
 
     <?= $form->field($model, 'alumni_school_address')->textInput(['maxlength' => 45]) ?>
 
