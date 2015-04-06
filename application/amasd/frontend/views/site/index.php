ef47b51053fa55e938652d4ba53f4014d18dce0d
jcabisado <abisado.jandra@gmail.com>
2015-04-07 07:55:47 +0800

    Commit amasd

---
+++ application/amasd/frontend/views/site/index.php
@@ -0,0 +1,113 @@
+<?php
+/* @var $this yii\web\View */
+$this->title = 'AMA Foundation';
+?>
+<div class="site-index">
+
+    <!--<div class="jumbotron">
+        <h1>Congratulations!</h1>
+
+        <p class="lead">You have successfully created your Yii-powered application.</p>
+
+        <p><a class="btn btn-lg btn-success" href="http://www.yiiframework.com">Get started with Yii</a></p>
+    </div>-->
+
+    <!--<div class="body-content">
+
+        <div class="row">-->
+	<!-- slides --> 
+	
+	<br><br><!--<body style="text-align: center;">-->
+	<!-- Start cssSlider.com -->
+	<center>
+	<div class='csslider1 autoplay '>
+		<input name="cs_anchor1" id='cs_slide1_0' type="radio" class='cs_anchor slide' >
+		<input name="cs_anchor1" id='cs_slide1_1' type="radio" class='cs_anchor slide' >
+		<input name="cs_anchor1" id='cs_slide1_2' type="radio" class='cs_anchor slide' >
+		<input name="cs_anchor1" id='cs_play1' type="radio" class='cs_anchor' checked>
+		<input name="cs_anchor1" id='cs_pause1' type="radio" class='cs_anchor' >
+		<ul>
+			<div style="width: 100%; visibility: hidden; font-size: 0px; line-height: 0;">
+				<img src="http://s11.postimg.org/h6oe4x6ab/image1.jpg" style="width: 100%;">
+			</div>
+			<li class='num0 img'>
+				 <a href="http://cssslider.com" target=""><img src='http://s11.postimg.org/h6oe4x6ab/image1.jpg' alt='Buns' title='Buns' /> </a> 
+			</li>
+			<li class='num1 img'>
+				 <a href="http://cssslider.com" target=""><img src='http://s18.postimg.org/fycju2omh/image3.jpg' alt='Croissant' title='Croissant' /> </a> 
+			</li>
+			<li class='num2 img'>
+				 <a href="http://cssslider.com" target=""><img src='http://s4.postimg.org/6w9ql5svh/carousel4.jpg' alt='Lemon pie' title='Lemon pie' /> </a> 
+			</li>
+		
+		</ul>
+		<div class='cs_description'>
+			<label class='num0'>
+				<span class="cs_title"><span class="cs_wrapper">AMA</span></span>
+			</label>
+			<label class='num1'>
+				<span class="cs_title"><span class="cs_wrapper">AMA</span></span>
+			</label>
+			<label class='num2'>
+				<span class="cs_title"><span class="cs_wrapper">AMA</span></span>
+			</label>
+		</div>
+		
+		<div class='cs_arrowprev'>
+			<label class='num0' for='cs_slide1_0'></label>
+			<label class='num1' for='cs_slide1_1'></label>
+			<label class='num2' for='cs_slide1_2'></label>
+		</div>
+		<div class='cs_arrownext'>
+			<label class='num0' for='cs_slide1_0'></label>
+			<label class='num1' for='cs_slide1_1'></label>
+			<label class='num2' for='cs_slide1_2'></label>
+		</div>
+		
+		<div class='cs_bullets'>
+			<label class='num0' for='cs_slide1_0'>
+				<span class='cs_point'></span>
+				<span class='cs_thumb'><img src='http://s15.postimg.org/nvcbrqhrr/image1.jpg' alt='Buns' title='Buns' /></span>
+			</label>
+			<label class='num1' for='cs_slide1_1'>
+				<span class='cs_point'></span>
+				<span class='cs_thumb'><img src='http://s14.postimg.org/j3yck30a5/image3.jpg' alt='Croissant' title='Croissant' /></span>
+			</label>
+			<label class='num2' for='cs_slide1_2'>
+				<span class='cs_point'></span>
+				<span class='cs_thumb'><img src='http://s29.postimg.org/eu1bwq98z/carousel4.jpg' alt='Lemon pie' title='Lemon pie' /></span>
+			</label>
+		</div>
+		</div></center>
+		<!-- End cssSlider.com -->
+  
+  <br />
+  </body>
+  <!--slides-->
+  
+			<br>
+            <div class="col-lg-4">
+                <h2>Who we are?</h2>
+
+                <p>The Associate Missionaries of the Assumption is a community of young volunteers who are committed to transforming society through joyful service deeply rooted in faith and prayer.</p>
+
+                <!--<p><a class="btn btn-default" href="http://www.yiiframework.com/doc/">Yii Documentation &raquo;</a></p>-->
+            </div>
+            <div class="col-lg-4">
+                <h2>What is AMA?</h2>
+
+                <p>In 1954, the Assumption Sisters founded the Associate Missionaries of the Assumption (AMA) as a way of answering the many appeals of the world. Today, the work continues. While at their volunteer sites, AMA's find new insight into God's gifts through service to others. It is through their service that they come to understand more fully the rule of life of The Religious of the Assumption: "Love never remains idle: it makes us creative and impels us to heed all the appeals of the world and to find new ways of answering them."</p>
+
+                <!--<p><a class="btn btn-default" href="http://www.yiiframework.com/forum/">Yii Forum &raquo;</a></p>-->
+            </div>
+            <div class="col-lg-4">
+                <h2>What we do?</h2>
+
+                <p>The alumni community continues to be a support group, has provided human & material resources to the program. They oversee implementation & sustainability of the organization.</p>
+
+                <!--<p><a class="btn btn-default" href="http://www.yiiframework.com/extensions/">Yii Extensions &raquo;</a></p>-->
+            </div>
+        </div>
+
+    </div>
+</div>
