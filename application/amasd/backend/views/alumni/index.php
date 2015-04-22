9c0e72889dc0d7edfab7a25949d1ae59d31a9165
jcabisado <abisado.jandra@gmail.com>
2015-04-23 00:27:37 +0800

    Commit alumni export file and print

--- application/amasd/backend/views/alumni/index.php
+++ application/amasd/backend/views/alumni/index.php
@@ -1,29 +1,21 @@
 <?php
 
 use yii\helpers\Html;
-use yii\grid\GridView;
+//use yii\grid\GridView;
+use kartik\grid\GridView;
 
 /* @var $this yii\web\View */
 /* @var $searchModel backend\models\alumniSearch */
 /* @var $dataProvider yii\data\ActiveDataProvider */
 
-$this->title = 'Alumnis';
+$this->title = 'Alumni';
 $this->params['breadcrumbs'][] = $this->title;
 ?>
 <div class="alumni-index">
 
-    <h1><?= Html::encode($this->title) ?></h1>
-    <?php // echo $this->render('_search', ['model' => $searchModel]); ?>
-
-    <p>
-        <?= Html::a('Create Alumni', ['create'], ['class' => 'btn btn-success']) ?>
-    </p>
-
-    <?= GridView::widget([
-        'dataProvider' => $dataProvider,
-        'filterModel' => $searchModel,
-        'columns' => [
-            ['class' => 'yii\grid\SerialColumn'],
+   <?php 
+    $gridColumns = [
+        ['class' => 'kartik\grid\SerialColumn'],
 
             //'id',
             'alumni_lname',
@@ -54,8 +46,27 @@ $this->params['breadcrumbs'][] = $this->title;
             // 'alumni_spouse',
             // 'alumni_num_children',
 
-            ['class' => 'yii\grid\ActionColumn'],
+             ['class' => 'kartik\grid\ActionColumn'],
+            ];
+			
+    echo GridView::widget([
+        'dataProvider'=> $dataProvider,
+        'filterModel' => $searchModel,
+        'columns' => $gridColumns,
+        'responsive'=>true,
+        'hover'=>true,
+        'containerOptions' => ['style'=>'overflow: auto'], // only set when $responsive = false
+        'headerRowOptions'=>['class'=>'kartik-sheet-style'],
+        'filterRowOptions'=>['class'=>'kartik-sheet-style'],
+        'pjax' => true, 
+        'floatHeader'=>true,
+        'panel' => [
+            'heading'=>'<h3 class="panel-title"><i class="glyphicon glyphicon-user"></i>&nbsp&nbsp Alumni</h3>',
+            'type'=>GridView::TYPE_PRIMARY,
+            'before'=>Html::a('<i class="glyphicon glyphicon-plus"></i> Add New Alumna', ['create'], ['class' => 'btn btn-success']),
+            'after'=>Html::a('<i class="glyphicon glyphicon-repeat"></i> Reset Grid', ['index'], ['class' => 'btn btn-info']),
         ],
-    ]); ?>
+    ]); 
+    ?>
 
 </div>
