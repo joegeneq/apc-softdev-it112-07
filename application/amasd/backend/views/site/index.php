e21baeaef1af14aa9150d1c71a94f2bc94b3b8bc
jcabisado <abisado.jandra@gmail.com>
2015-04-23 03:00:49 +0800

    Commit backend updates and vendor

--- application/amasd/backend/views/site/index.php
+++ application/amasd/backend/views/site/index.php
@@ -1,50 +1,54 @@
 <?php
 /* @var $this yii\web\View */
 
-$this->title = 'My Yii Application';
+$this->title = 'Home';
 ?>
+<style type="text/css"></style>
 <div class="site-index">
 
-    <div class="jumbotron">
+    <!--<div class="jumbotron">
         <h1>Congratulations!</h1>
 
         <p class="lead">You have successfully created your Yii-powered application.</p>
 
         <p><a class="btn btn-lg btn-success" href="http://www.yiiframework.com">Get started with Yii</a></p>
-    </div>
+    </div>-->
 
-    <div class="body-content">
+    <!--<div class="body-content">
 
-        <div class="row">
+        <div class="row">-->
+	<!-- slides --> 
+	
+	<br><br><!--<body style="text-align: center;">-->
+	<center>
+	<h1>Associate Missionaries of the Assumption Foundation</h1>
+	</center><br>
+  <!--slides-->
+  
+			<br>
             <div class="col-lg-4">
-                <h2>Heading</h2>
+				<img src='http://s11.postimg.org/h6oe4x6ab/image1.jpg' alt='Buns' title='Buns' /> </a> <br>
+                <h2>Who we are?</h2>
 
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
-                    dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
-                    ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
-                    fugiat nulla pariatur.</p>
+                <p>The Associate Missionaries of the Assumption is a community of young volunteers who are committed to transforming society through joyful service deeply rooted in faith and prayer.</p>
 
-                <p><a class="btn btn-default" href="http://www.yiiframework.com/doc/">Yii Documentation &raquo;</a></p>
+                <!--<p><a class="btn btn-default" href="http://www.yiiframework.com/doc/">Yii Documentation &raquo;</a></p>-->
             </div>
             <div class="col-lg-4">
-                <h2>Heading</h2>
+                <img src='http://s4.postimg.org/6w9ql5svh/carousel4.jpg' alt='Lemon pie' title='Lemon pie' /> <br></a> 
+				<h2>What is AMA?</h2>
 
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
-                    dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
-                    ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
-                    fugiat nulla pariatur.</p>
+                <p>In 1954, the Assumption Sisters founded the Associate Missionaries of the Assumption (AMA) as a way of answering the many appeals of the world. Today, the work continues. While at their volunteer sites, AMA's find new insight into God's gifts through service to others. It is through their service that they come to understand more fully the rule of life of The Religious of the Assumption: "Love never remains idle: it makes us creative and impels us to heed all the appeals of the world and to find new ways of answering them."</p>
 
-                <p><a class="btn btn-default" href="http://www.yiiframework.com/forum/">Yii Forum &raquo;</a></p>
+                <!--<p><a class="btn btn-default" href="http://www.yiiframework.com/forum/">Yii Forum &raquo;</a></p>-->
             </div>
             <div class="col-lg-4">
-                <h2>Heading</h2>
+				<img src='http://s18.postimg.org/fycju2omh/image3.jpg' alt='Croissant' title='Croissant' /> <br></a> 
+                <h2>What we do?</h2>
 
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
-                    dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
-                    ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
-                    fugiat nulla pariatur.</p>
+                <p>The alumni community continues to be a support group, has provided human & material resources to the program. They oversee implementation & sustainability of the organization.</p>
 
-                <p><a class="btn btn-default" href="http://www.yiiframework.com/extensions/">Yii Extensions &raquo;</a></p>
+                <!--<p><a class="btn btn-default" href="http://www.yiiframework.com/extensions/">Yii Extensions &raquo;</a></p>-->
             </div>
         </div>
 
