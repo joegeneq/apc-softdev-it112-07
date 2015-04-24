82f4d28c73a593e7ba598bf28788b96b43ffda73
jcabisado <abisado.jandra@gmail.com>
2015-04-25 00:05:19 +0800

    Commit update in backend asset

--- application/amasd/backend/assets/AppAsset.php
+++ application/amasd/backend/assets/AppAsset.php
@@ -15,14 +15,15 @@ use yii\web\AssetBundle;
  */
 class AppAsset extends AssetBundle
 {
-    //public $basePath = '@webroot';
-    public $sourcePath = '@bower/';
-	//public $baseUrl = '@web';
-    public $css = ['admin-lte/css/AdminLTE.css'];
-    public $js = [ 'admin-lte/js/AdminLTE/app.js'];
+    public $basePath = '@webroot';
+    public $baseUrl = '@web';
+    public $css = [
+        'css/site.css',
+    ];
+    public $js = [
+    ];
     public $depends = [
         'yii\web\YiiAsset',
         'yii\bootstrap\BootstrapAsset',
-        'yii\bootstrap\BootstrapPluginAsset',
     ];
 }
